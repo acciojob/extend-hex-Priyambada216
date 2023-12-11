eea737aa27bcc70290045df5ec3a2fcdab983443
Priyambada216 <144552804+Priyambada216@users.noreply.github.com>
2023-12-11 19:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Check the length of the short hex string
+  if (shortHex.length === 1) {
+    // If the length is 1, repeat the character 6 times
+    return shortHex.repeat(6);
+  } else if (shortHex.length === 3) {
+    // If the length is 3, repeat the string 2 times
+    return shortHex.repeat(2);
+  } else {
+    // If the length is not 1 or 3, return the original string
+    return shortHex;
+  }
 };
 
 // Do not change the code below.
